9a1e7aaa74decd7401e1f5fcf0f9b506aa49556a
wbw <wbw262174@gmail.com>
2022-06-11 23:43:44 +0800

    [fix]1.mapper.xml 实体类映射错误修复

--- user-center/src/main/java/com/wbw/cloud/UserCenterApplication.java
+++ user-center/src/main/java/com/wbw/cloud/UserCenterApplication.java
@@ -1,17 +1,26 @@
 package com.wbw.cloud;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author wbw
  * @since 2022-6-5 19:32
  */
 @EnableDiscoveryClient
+@MapperScan(basePackages = "com.wbw.cloud.dao")
 @SpringBootApplication
 public class UserCenterApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class, args);
+
     }
+
 }
--- user-center/src/test/java/com/wbw/cloud/dao/WechatDaoTest.java
+++ user-center/src/test/java/com/wbw/cloud/dao/WechatDaoTest.java
@@ -2,11 +2,14 @@ package com.wbw.cloud.dao;
 
 import com.wbw.cloud.model.user.WechatUserInfo;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import java.util.Date;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class WechatDaoTest {
     @Autowired
@@ -14,26 +17,29 @@ public class WechatDaoTest {
 
     @Test
     public void findByOpenid() {
-        wechatDao.findByOpenid("asasa");
+        System.out.println(wechatDao.findByOpenid("asasa"));
     }
 
     @Test
     public void findByUnionId() {
+        wechatDao.findByUnionId("212121w").forEach(System.out::println);
     }
 
     @Test
     public void insert() {
         WechatUserInfo wechatUserInfo = new WechatUserInfo();
-        wechatUserInfo.setOpenid("asasa");
+        wechatUserInfo.setOpenid("qqq");
         wechatUserInfo.setUnionid("212121w");
+        wechatUserInfo.setApp("12112312");
+        wechatUserInfo.setNickname("aaa");
+        wechatUserInfo.setCreatetime(new Date());
+        wechatUserInfo.setUpdatetime(new Date());
         wechatDao.insert(wechatUserInfo);
     }
 
     @Test
-    public void update() {
-    }
-
-    @Test
     public void deleteById() {
+        wechatDao.deleteById(1);
+        wechatDao.deleteById(3);
     }
 }
